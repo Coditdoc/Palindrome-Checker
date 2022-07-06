b8cd8ed8dd044e6458c8bc5efced8d9c329c3f72
Codeitdoc7 <99911985+Coditdoc@users.noreply.github.com>
2022-07-06 21:54:43 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,43 +1,42 @@
+        
     const strarr = [];
     let revearr = [];
     let userstr = document.getElementById("Udt1");
     
     userstr.addEventListener("keydown", function (e) {
         if (e.key === "Enter") {
-            ente(e);
+            validate(e);
         }
     });
     
-    function ente(e) {
+    function validate(e) {
         tex()
     }
     
-    
-    
     function tex() {
       console.clear();
+      document.getElementById("udans").style.visibility="visible";
       if (userstr.value == "") {
         document.getElementById("udans").innerHTML = "haha nice try :)";
-        console.log("Hey feed me some text, I'm hungry")
+        console.log("enter text")
       }
-      else {
-        
+      else if (userstr.value.length <= 2) {
+        document.getElementById("udans").innerHTML = "feed me more than 3 words";    
+      }
+    
+      else{
         let strupr = userstr.value.toUpperCase();
-        
         let rege = /[a-z0-9]/gi;
         let renonalp = strupr.match(rege);
-        
     
         for (let i = 0; i < renonalp.length; i++) {
           strarr.push(renonalp[i]);
         }
     
-        
         for (let i = 0; i < strarr.length; i++) {
           revearr.push(strarr[i]);
         }
         revearr.reverse();
-        
         if (JSON.stringify(strarr) == JSON.stringify(revearr)) {
           document.getElementById("udans").innerHTML = "Palindrome";
           console.log("equal");
